58deeb4bea7fa1dc3a04449419d46772e96cbf74
rmlebioda <lebioda.rafal@gmail.com>
2022-03-13 22:31:39 +0100

    update to guildstats

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from selenium import webdriver
 from webdriver.WebDriverWrapper import WebDriverWrapper
 
 if __name__ == '__main__':
-    worlds = ["Adra", "Antica", "Bona", "Celesta", "Damora", "Famosa", "Harmonia", "Karna",
+    yetis_worlds = ["Adra", "Antica", "Bona", "Celesta", "Damora", "Famosa", "Harmonia", "Karna",
               "Marcia", "Monza", "Olima", "Peloria", "Refugia", "Secura", "Suna", "Thyria", "Vunira"]
+    ocyakao_worlds = ['Antica', 'Bona', 'Celesta', 'Damora', 'Famosa', 'Harmonia', 'Monza',
+                'Peloria', 'Premia', 'Refugia', 'Secura', 'Suna', 'Thyria', 'Vunira']
 
     print("Starting...")
     chrome_options = webdriver.ChromeOptions()
@@ -13,9 +15,10 @@ if __name__ == '__main__':
 
     wrapper = WebDriverWrapper(webdriver)
 
-    bosses = wrapper.get_boss_data('yetis', worlds=worlds)
+    bosses = wrapper.get_boss_data('yetis', worlds=yetis_worlds)
+    bosses += wrapper.get_boss_data('Ocyakao', worlds=ocyakao_worlds)
 
-    bosses.sort(key=lambda x: x.data.extract_chances(), reverse=True)
+    bosses.sort(key=lambda x: (x.name, x.data.get_chances_text_value(), x.data.get_chances(), x.data.last_seen_days()), reverse=True)
 
     for boss in bosses:
         print(boss)
--- models/boss_data.py
+++ models/boss_data.py
@@ -1,3 +1,5 @@
+from distutils.util import change_root
+from itertools import chain
 import re
 
 from attr import dataclass
@@ -11,12 +13,42 @@ class BossChances:
     last_seen: str
     last_update: str
 
-    def extract_chances(self) -> float:
+    def get_chances(self) -> float:
+        """Returns numeric value of odds for boss to spawn
+
+        Returns:
+            float: chances for boss to spawn
+        """
         chances = re.findall(r"\d+\.\d+", self.chances)
         return float(chances[0]) if len(chances) == 1 else float(0)
 
+    def get_chances_text_value(self) -> int:
+        """Returns numeric value of chances, the higher number, the higher ods of it's spawning, can be useful for sorting
+
+        Returns:
+            int: numeric value represeting importance of chances for the boss to spawn
+        """
+        if self.chances.lower().startswith('high chance'):
+            return 2
+        elif self.chances.lower().startswith('low'):
+            return 1
+        elif self.chances.lower().startswith('No chance'):
+            return 0
+        return -1
+
     def __str__(self):
         return f'world: {self.world} \tchances: {self.chances}\t{self.last_seen}\t({self.last_update})'
+
+    def last_seen_days(self) -> int:
+        """Returns number of days last seen of boss (or 0 in case of error)
+
+        Returns:
+            int: last seen days value
+        """
+        days = re.findall(r"(\d+) days", self.last_seen)
+        if len(days) > 0:
+            return int(days[0])
+        return 0
 
 @dataclass
 class BossData:
